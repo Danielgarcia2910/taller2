0f6daeeff929e81952acbb16fa4de7118820a449
Danielgarcia2910 <garciajimenezdanieljose@gmail.com>
2026-05-08 17:31:07 -0500

    Update ProductoNoEncontradoException.java

--- src/main/java/Modelo/excepciones/ProductoNoEncontradoException.java
+++ src/main/java/Modelo/excepciones/ProductoNoEncontradoException.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modelo.excepciones;
+package modelo.excepciones;
 
-/**
- *
- * @author Orly02
- */
+public class ProductoNoEncontradoException extends Exception {
 
-public class EstadoTransaccionInvalidoException extends SistemaBancarioException{
-
-    public EstadoTransaccionInvalidoException(String mensaje) {
+    public ProductoNoEncontradoException(String mensaje) {
         super(mensaje);
     }
 }
