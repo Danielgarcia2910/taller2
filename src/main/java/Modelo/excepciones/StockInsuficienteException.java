25076c1c867dbe76e607f8b18bbdc79cb2d38aa5
Danielgarcia2910 <garciajimenezdanieljose@gmail.com>
2026-05-08 17:31:50 -0500

    Update StockInsuficienteException.java

--- src/main/java/Modelo/excepciones/StockInsuficienteException.java
+++ src/main/java/Modelo/excepciones/StockInsuficienteException.java
@@ -1,35 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modelo.excepciones;
+package modelo.excepciones;
 
-/**
- *
- * @author Orly02
- */
-public class SaldoInsuficienteException extends SistemaBancarioException {
+public class StockInsuficienteException extends Exception {
 
-    private double saldoActual;
-    private double montoSolicitado;
-
-    public SaldoInsuficienteException(String mensaje, double saldoActual, double montoSolicitado) {
+    public StockInsuficienteException(String mensaje) {
         super(mensaje);
-        this.saldoActual = saldoActual;
-        this.montoSolicitado = montoSolicitado;
-    }
-
-    public double getSaldoActual() {
-        return saldoActual;
-    }
-
-    public double getMontoSolicitado() {
-        return montoSolicitado;
-    }
-
-    @Override
-    public String toString() {
-        return "SaldoInsuficienteException{" + "saldoActual=" + saldoActual + ", montoSolicitado=" + montoSolicitado +", mensaje=" + getMessage() +
-               '}';
     }
 }
