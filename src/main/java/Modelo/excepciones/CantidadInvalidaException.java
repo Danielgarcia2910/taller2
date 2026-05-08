dd989e88e628d013c2c02900bc73053c42624b42
Danielgarcia2910 <garciajimenezdanieljose@gmail.com>
2026-05-08 17:29:12 -0500

    Update CantidadInvalidaException.java

--- src/main/java/Modelo/excepciones/CantidadInvalidaException.java
+++ src/main/java/Modelo/excepciones/CantidadInvalidaException.java
@@ -1,21 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modelo.excepciones;
+package modelo.excepciones;
 
+public class CantidadInvalidaException extends Exception {
 
-
-import java.time.LocalDateTime;
-
-public class SistemaBancarioException extends Exception {
-
-    protected String codigoError;
-    protected LocalDateTime timestamp;
-
-    public SistemaBancarioException(String mensaje) {
+    public CantidadInvalidaException(String mensaje) {
         super(mensaje);
-        this.timestamp = LocalDateTime.now();
-        this.codigoError = "ERR-" + System.currentTimeMillis();
     }
 }
